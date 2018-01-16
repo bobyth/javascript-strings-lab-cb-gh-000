27409ca7ccc3a7d79cad480fa2c22744c98916dd
jane smith <bobythan@gmail.com>
2018-01-16 20:57:01 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 var greeting ='Hello, everybody!';
-var specialGuest = "neil deGrasse Tyson";
+var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 var conversation = `${topic}`;
