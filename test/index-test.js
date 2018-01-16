b0cc4b1fe35bd859e33e6402efada70b6aac0c4d
jane smith <bobythan@gmail.com>
2018-01-16 21:04:10 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,7 @@ var greeting ='Hello, everybody!';
 var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
-var conversation = `Let's talk about ${topic}`;
+var conversation = `'Let\'s talk about ${topic}`;
 
 
 it('defines `greeting`', function() {
